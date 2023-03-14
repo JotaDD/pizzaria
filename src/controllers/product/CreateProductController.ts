9305fcd526c0bd7923e08762b1e16483b453907d
JotaDD <joaoeduardogomes@gmail.com>
2023-03-14 17:07:45 -0300

    bug fix

--- src/controllers/product/CreateProductController.ts
+++ src/controllers/product/CreateProductController.ts
@@ -4,30 +4,24 @@ import { CreateProductService } from "../../services/product/CreateProductServic
 class CreateProductController {
   async handle(req: Request, res: Response) {
     const { name, price, description, category_id } = req.body;
-                
+
     const createProductService = new CreateProductService();
 
-    if(!req.file){
-      throw new Error("Erro ao fazer o upload do arquivo de foto")
-    }else{
-
-      const {originalname, filename} = req.file
+    if (!req.file) {
+      throw new Error("Erro ao fazer o upload do arquivo de foto");
+    } else {
+      const { originalname, filename } = req.file;
       console.log(filename);
-      
 
       const product = await createProductService.execute({
         name,
         price,
         description,
-        banner: '',
+        banner: "",
         category_id,
       });
-      
+      return res.json(product);
     }
-
-    
-    
-    return res.json(product)
   }
 }
 
