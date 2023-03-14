61656530024ea00d94ebdada880693cc0696fc96
JotaDD <joaoeduardogomes@gmail.com>
2023-03-14 14:49:17 -0300

    add create category

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 import { isAuthenticated } from "./middlewares/isAuthenticated"
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController"
 
 
 const router = Router();
@@ -11,8 +12,12 @@ const router = Router();
 //-- ROTAS USER --
 router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
-
-
 router.get("/me", isAuthenticated ,new DetailUserController().handle);
+
+//-- ROTAS CATEGORY -- 
+
+router.post("/category", isAuthenticated, new CreateCategoryController().handle)
+
+
 
 export { router };
