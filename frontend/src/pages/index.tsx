1549c96a032cd6c5a81d196f4b28c7ad2425cfa5
JotaDD <joaoeduardogomes@gmail.com>
2023-03-20 13:51:30 -0300

    add input component

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,8 +1,26 @@
+import Head from "next/head"
+import Image from "next/image"
+import styles from '../styles/home.module.scss'
+import logoImg from '../../public/logo.png'
+
+import { Input } from "../components/ui/input"
 
 export default function Home() {
   return (
-   <div>
-    <h1>Pagina NextJS</h1>
-   </div>
+    <>
+      <Head>
+        <title> Corleone Pizza - Faça seu login</title>
+      </Head>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg}  className={styles.logo}  alt="Logo Corleone Pizza" />
+
+        <div className={styles.login}>
+          <form>
+            <Input placeholder="Digite seu email" type="text"/>
+            <Input placeholder="Sua Senha" type="password" />
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
