cda76c08eb9115b4ef8f3b2ae7e31be3d28e01d3
JotaDD <joaoeduardogomes@gmail.com>
2023-03-20 16:35:59 -0300

    fixing logo

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head"
 import Image from "next/image"
 import styles from '../styles/home.module.scss'
-import logoImg from '../../public/logo.png'
+import logoImg from '../../public/logo.svg'
 
 import { Input } from "../components/ui/input"
 
