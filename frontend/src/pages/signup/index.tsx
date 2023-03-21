2f82b8b07fcde70b064c81d5768d9ec506f145b0
JotaDD <joaoeduardogomes@gmail.com>
2023-03-21 13:26:18 -0300

    add SignUp

--- frontend/src/pages/signup/index.tsx
+++ frontend/src/pages/signup/index.tsx
@@ -6,8 +6,43 @@ import logoImg from '../../../public/logo.svg'
 import Link from "next/link"
 import Button from "../../components/ui/Button"
 import { Input } from "../../components/ui/Input"
+import { FormEvent, useState } from "react"
 
-export default function Signup() {
+export default function SignUp() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [loading, setLoading] = useState(false)
+
+
+  const handleSignUp = async (event: FormEvent) => {
+    event.preventDefault()
+
+    if (name === '' || email === '' || password === '') {
+      alert("PREENCHA TODOS OS CAMPOS")
+      return
+    }
+    setLoading(true)
+
+
+  }
+  const handleName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value)
+
+  }
+  const handleEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value)
+
+  }
+  const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value)
+
+  }
+
+
+
+
   return (
     <>
       <Head>
@@ -18,14 +53,14 @@ export default function Signup() {
 
         <div className={styles.login}>
           <h1>Criando sua conta</h1>
-          <form>
-            <Input placeholder="Digite seu nome" type="text" />
+          <form onSubmit={handleSignUp}>
+            <Input value={name} onChange={handleName} placeholder="Digite seu nome" type="text" />
 
-            <Input placeholder="Digite seu email" type="text" />
+            <Input value={email} onChange={handleEmail} placeholder="Digite seu email" type="text" />
 
-            <Input placeholder="Sua Senha" type="password" />
+            <Input value={password} onChange={handlePassword} placeholder="Sua Senha" type="password" />
 
-            <Button type="submit" loading={false}>Cadastrar</Button>
+            <Button type="submit" loading={loading}>Cadastrar</Button>
           </form>
           <Link className={styles.text} href="/">
             Já possui uma conta? Faça login
