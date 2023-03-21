a738e1ab47dd16df593b6d22e1fd9dc24466be73
JotaDD <joaoeduardogomes@gmail.com>
2023-03-21 10:06:26 -0300

    add AuthContext

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,13 +1,31 @@
 import Head from "next/head"
 import Image from "next/image"
+import Link from "next/link"
+
 import styles from '../styles/home.module.scss'
 import logoImg from '../../public/logo.svg'
 
+import { AuthContext } from "../contexts/AuthContext"
+import { useContext, FormEvent } from 'react'
+
 import { Input } from "../components/ui/Input"
 import Button from "../components/ui/Button"
-import Link from "next/link"
+
 
 export default function Home() {
+  const { signIn } = useContext(AuthContext)
+
+  const handleLogin = async (event: FormEvent) =>{
+    event.preventDefault()
+
+    let data = {
+      email: "teste@teste.com",
+      password:"123123"
+    }
+
+    await signIn(data)
+
+  }
   return (
     <>
       <Head>
@@ -17,7 +35,7 @@ export default function Home() {
         <Image src={logoImg} className={styles.logo} alt="Logo Corleone Pizza" />
 
         <div className={styles.login}>
-          <form>
+          <form onSubmit={handleLogin}>
             <Input placeholder="Digite seu email" type="text" />
 
             <Input placeholder="Sua Senha" type="password" />
